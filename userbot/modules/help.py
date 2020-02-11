ddfcb87020400719d63b20c8739ded0728d97e3b
HitaloKun <hitalomarquete331@gmail.com>
2020-02-11 14:27:14 -0300

    Reverting to the old help module

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -5,32 +5,24 @@
 #
 """ Userbot help command """
 
-import textwrap
-
 from userbot import CMD_HELP
 from userbot.events import register
 
-CAT_ITEMS = {}
 
-
-@register(outgoing=True, pattern=r"^\.help(?: |$)(.*)")
-async def show_help(event):
+@register(outgoing=True, pattern="^\.help(?: |$)(.*)")
+async def help(event):
     """ For .help command,"""
-    args = event.pattern_match.group(1)
+    args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**Please specify a valid module name.**", delete_in=3)
+            await event.edit("Please specify a valid module name.")
     else:
-        categories = list(CAT_ITEMS.keys())
-        categories.sort()
-
-        categorized = []
-        for cat in categories:
-            cat_items = sorted(CAT_ITEMS[cat])
-            msg = f"**{cat}** \n```{', '.join(cat_items)}```"
-            categorized.append(msg)
-
-        message = "**Please specify which module do you want help for!** \n\n" + '\n\n'.join(categorized)
-        await event.edit(message)
+        await event.edit("Please specify which module do you want help for !!\
+            \nUsage: .help <module name>")
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\n"
+        await event.reply(string)
