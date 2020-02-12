184b1ef1070e93f04dfd8b680529edd03f893c2b
HitaloKun <hitalomarquete331@gmail.com>
2020-02-12 09:38:00 -0300

    Improvements

--- userbot/modules/misc/github.py
+++ userbot/modules/misc/github.py
@@ -118,7 +118,7 @@ async def build_repo_message(repo, args):
 
 
 add_help_item(
-    ".gh",
+    "github",
     "Misc",
     "Displays information related to a github repo. "
     "Similar to `.user`.",
