7cebadf4de95afb4c9e81baaf35433a17602c350
HitaloKun <hitalomarquete331@gmail.com>
2020-02-13 19:09:37 -0300

    Added new warn module from UniBorg

---
+++ userbot/modules/admin/warns.py
@@ -0,0 +1,83 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+import asyncio
+import html
+from telethon import events
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+import sql_helpers.warns_sql as sql
+from ..help import add_help_item
+
+
+@register(outgoing=True, pattern=r"^\.warn (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    warn_reason = event.pattern_match.group(1)
+    reply_message = await event.get_reply_message()
+    limit, soft_warn = sql.get_warn_setting(event.chat_id)
+    num_warns, reasons = sql.warn_user(reply_message.from_id, event.chat_id, warn_reason)
+    if num_warns >= limit:
+        sql.reset_warns(reply_message.from_id, event.chat_id)
+        if soft_warn:
+            logger.info("TODO: kick user")
+            reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been kicked!".format(limit, reply_message.from_id)
+        else:
+            logger.info("TODO: ban user")
+            reply = "{} warnings, <u><a href='tg://user?id={}'>user</a></u> has been banned!".format(limit, reply_message.from_id)
+    else:
+        reply = "<u><a href='tg://user?id={}'>user</a></u> has {}/{} warnings... watch out!".format(reply_message.from_id, num_warns, limit)
+        if warn_reason:
+            reply += "\nReason for last warn:\n{}".format(html.escape(warn_reason))
+    #
+    await event.edit(reply, parse_mode="html")
+
+
+@register(outgoing=True, pattern="^\.warns$")
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    result = sql.get_warns(reply_message.from_id, event.chat_id)
+    if result and result[0] != 0:
+        num_warns, reasons = result
+        limit, soft_warn = sql.get_warn_setting(event.chat_id)
+        if reasons:
+            text = "This user has {}/{} warnings, for the following reasons:".format(num_warns, limit)
+            text += "\r\n"
+            text += reasons
+            await event.edit(text)
+        else:
+            await event.edit("this user has {} / {} warning, but no reasons for any of them.".format(num_warns, limit))
+    else:
+        await event.edit("this user hasn't got any warnings!")
+
+
+@register(outgoing=True, pattern="^\.resetwarns$")
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_message = await event.get_reply_message()
+    sql.reset_warns(reply_message.from_id, event.chat_id)
+    await event.edit("Warnings have been reset!")
+
+
+add_help_item(
+    "warns",
+    "Admin",
+    "Userbot module containing commands for warn users",
+    """
+.warn
+Usage: Wane a user.
+
+.warns
+Usage: See all warns for a user.
+
+.resetwarns
+Usage: Reset all warns of a user.
+    """
+)
