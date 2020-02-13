ff55efddc76a17721747f3a8b948193d91fc0327
HitaloKun <hitalomarquete331@gmail.com>
2020-02-13 12:15:19 -0300

    Fixes for gban module

--- userbot/modules/admin/gban.py
+++ userbot/modules/admin/gban.py
@@ -10,12 +10,13 @@ from telethon import events
 import asyncio
 from userbot.events import register
 from userbot import bot
+from userbot import GBAN_GROUP
 from ..help import add_help_item
 
 
-@register(outgoing=True, pattern=r"^\.gban(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.fban(?: |$)(.*)")
 async def _(event):
-    if Config.GBAN_GROUP is None:
+    if GBAN_GROUP is None:
         await event.edit("ENV VAR is not set. This module will not work.")
         return
     if event.fwd_from:
@@ -27,16 +28,15 @@ async def _(event):
             r_from_id = r.forward.from_id or r.from_id
         else:
             r_from_id = r.from_id
-        await bot.send_message(
-            Config.GBAN_GROUP,
+        await bot.send_message(GBAN_GROUP,
             "/fban [user](tg://user?id={}) {}".format(r_from_id, reason)
         )
     await event.delete()
 
 
-@register(outgoing=True, pattern=r"^\.ungban(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.unfban(?: |$)(.*)")
 async def _(event):
-    if Config.GBAN_GROUP is None:
+    if GBAN_GROUP is None:
         await event.edit("ENV VAR is not set. This module will not work.")
         return
     if event.fwd_from:
@@ -45,8 +45,7 @@ async def _(event):
     if event.reply_to_msg_id:
         r = await event.get_reply_message()
         r_from_id = r.from_id
-        await bot.send_message(
-            Config.GBAN_GROUP,
+        await bot.send_message(GBAN_GROUP,
             "/unfban [user](tg://user?id={}) {}".format(r_from_id, reason)
         )
     await event.delete()
