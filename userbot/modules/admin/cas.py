7d89fd47f90fff484d78e8732454a6829f4838f6
HitaloKun <hitalomarquete331@gmail.com>
2020-02-15 13:12:44 -0300

    Added credits for @nunopenim on CAS module

--- userbot/modules/admin/cas.py
+++ userbot/modules/admin/cas.py
@@ -3,6 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
+# Adapted module from https://github.com/nunopenim/tguserbot
 
 from asyncio import sleep
 from datetime import datetime
