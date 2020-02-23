3ecdc748f4cbf494452ab1f905fbfbe2f6ab973d
HitaloKun <40531911+HitaloKun@users.noreply.github.com>
2020-02-23 12:49:28 -0300

    Removing duplicate codes

--- userbot/modules/misc/scrapers.py
+++ userbot/modules/misc/scrapers.py
@@ -50,37 +50,6 @@ TTS_LANG = "en"
 TRT_LANG = "en"
 
 
-@register(outgoing=True, pattern="^\.img (.*)")
-async def img_sampler(event):
-    """ For .img command, search and return images matching the query. """
-    await event.edit("Processing...")
-    query = event.pattern_match.group(1)
-    lim = findall(r"lim=\d+", query)
-    try:
-        lim = lim[0]
-        lim = lim.replace("lim=", "")
-        query = query.replace("lim=" + lim[0], "")
-    except IndexError:
-        lim = 2
-    response = google_images_download.googleimagesdownload()
- 
-    # creating list of arguments
-    arguments = {
-        "keywords": query,
-        "limit": lim,
-        "format": "jpg",
-        "no_directory": "no_directory"
-    }
- 
-    # passing the arguments to the function
-    paths = response.download(arguments)
-    lst = paths[0][query]
-    await event.client.send_file(
-        await event.client.get_input_entity(event.chat_id), lst)
-    rmtree(os.path.dirname(os.path.abspath(lst[0])))
-    await event.delete()
-
-
 @register(outgoing=True, pattern="^\.crblang (.*)")
 async def setlang(prog):
     global CARBONLANG
