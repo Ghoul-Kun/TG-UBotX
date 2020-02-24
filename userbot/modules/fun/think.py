d08d6e144903bb0aec0de35f531398f2bcd5a51e
HitaloKun <40531911+HitaloKun@users.noreply.github.com>
2020-02-24 14:25:44 -0300

    New think.py module

---
+++ userbot/modules/fun/think.py
@@ -0,0 +1,71 @@
+import asyncio
+
+from telethon import events
+from userbot.utils import admin_cmd
+
+from ..help import add_help_item
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^\.think(?: |$)(.*)')
+async def think(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.01
+    animation_ttl = range(0, 288)
+    input_str = event.pattern_match.group(1)
+    if input_str == "think":
+        await event.edit(input_str)
+        animation_chars = [
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING... 🤔"
+        ]
+
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 72])
+            
+
+add_help_item(
+    "think",
+    "Fun",
+    "Think about it yourself.",
+    """
+    `.think`
+    """
+)
